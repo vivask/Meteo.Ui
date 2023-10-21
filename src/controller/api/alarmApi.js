b1ab2ed0aac3f85bbe91855ae9ecec47e49491ef
vivask <vivask@mail.ru>
2023-10-21 08:31:56 +0300

    initial commit

--- src/controller/api/alarmApi.js
+++ src/controller/api/alarmApi.js
@@ -17,16 +17,19 @@ export async function getAlarms() {
  * @param settings
  */
 export function setAlarms(settings) {
-  settings.max_6814_nh3 = parseFloat(settings.max_6814_nh3);
-  settings.max_6814_no2 = parseFloat(settings.max_6814_no2);
-  settings.max_6814_co = parseFloat(settings.max_6814_co);
-  settings.min_ds18b20 = parseFloat(settings.min_ds18b20);
-  settings.max_ds18b20 = parseFloat(settings.max_ds18b20);
-  settings.min_bmx280_tempr = parseFloat(settings.min_bmx280_tempr);
-  settings.max_bmx280_tempr = parseFloat(settings.max_bmx280_tempr);
-  settings.max_rad_stat = parseFloat(settings.max_rad_stat);
-  settings.max_rad_dyn = parseFloat(settings.max_rad_dyn);
-  settings.max_ch2o = parseFloat(settings.max_ch2o);
-
-  jwtClient.put('/esp32/settings', settings);
+  jwtClient.get('/esp32/settings').then(({ success, result }) => {
+    if (success) {
+      result.max_6814_nh3 = parseFloat(settings.max_6814_nh3);
+      result.max_6814_no2 = parseFloat(settings.max_6814_no2);
+      result.max_6814_co = parseFloat(settings.max_6814_co);
+      result.min_ds18b20 = parseFloat(settings.min_ds18b20);
+      result.max_ds18b20 = parseFloat(settings.max_ds18b20);
+      result.min_bmx280_tempr = parseFloat(settings.min_bmx280_tempr);
+      result.max_bmx280_tempr = parseFloat(settings.max_bmx280_tempr);
+      result.max_rad_stat = parseFloat(settings.max_rad_stat);
+      result.max_rad_dyn = parseFloat(settings.max_rad_dyn);
+      result.max_ch2o = parseFloat(settings.max_ch2o);
+      jwtClient.put('/esp32/settings', result);
+    }
+  });
 }
